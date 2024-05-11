f3b296a4fef44bd210fecec5932058abe2e6512c
nan-wang <nan.wang@jina.ai>
2024-05-11 14:27:17 +0800

    chore: clean up

--- src/envs.py
+++ src/envs.py
@@ -4,18 +4,21 @@ from huggingface_hub import HfApi
 
 # Info to change for your repository
 # ----------------------------------
-TOKEN = os.environ.get("TOKEN")  # A read/write token for your org
+TOKEN = os.environ.get("TOKEN", "")  # A read/write token for your org
 
-OWNER = "nan"  # Change to your org - don't forget to create a results and request dataset, with the correct format!
+OWNER = "AIR-Bench"  # "nan"  # Change to your org - don't forget to create a results and request dataset, with the correct format!
 # ----------------------------------
 
 REPO_ID = f"{OWNER}/leaderboard"
-RESULTS_REPO = f"{OWNER}/results"
+# repo for storing the evaluation results
+RESULTS_REPO = f"{OWNER}/eval_results"
+# repo for submitting the evaluation
+SEARCH_RESULTS_REPO = f"{OWNER}/search_results"
 
 # If you setup a cache later, just change HF_HOME
 CACHE_PATH = os.getenv("HF_HOME", ".")
 
 # Local caches
-EVAL_RESULTS_PATH = "/Users/nanwang/Codes/huggingface/nan/results/demo-leaderboard"  #os.path.join(CACHE_PATH, "eval-results")
+EVAL_RESULTS_PATH = os.path.join(CACHE_PATH, "eval_results")
 
 API = HfApi(token=TOKEN)
